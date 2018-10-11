d1689fb3ea89a549da0759a81fe51671ca813696
Kong <kong@Kongs-MacBook-Pro.local>
2018-10-11 20:12:46 +0700

    new model income-info

---
+++ src/app/models/income-info.ts
@@ -0,0 +1,9 @@
+export class IncomeInfo {
+  Fullname: string;
+  Email: string;
+  BankAccountName: string;
+  BankAccountNumber: string;
+  TotalIncome: string;
+  SubmitDate: string;
+  ThaiCitizenID: string;
+}
