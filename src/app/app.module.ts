da7c8619e77a34a5e8f3a467aeed79a4856952a4
Kong <kong@Kongs-MacBook-Pro.local>
2018-10-23 13:39:23 +0700

    update shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
-export function HttpLoaderFactory(http: HttpClient) {
+export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
@@ -22,10 +22,11 @@ export function HttpLoaderFactory(http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })],
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,18 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusHighlightDirective } from './directives/status-highlight.directive';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, TranslateModule
   ],
   declarations: [
     StatusHighlightDirective,
   ],
   exports: [
     StatusHighlightDirective,
+    TranslateModule,
   ]
 })
 export class SharedModule {
