9b316453f7deda8835aa77537402cc57683ff783
Bordin Intarma <bordin.int@gmail.com>
2018-10-27 11:58:52 +0700

    add httpOption to base-api

--- src/app/layouts/services/base-api.service.ts
+++ src/app/layouts/services/base-api.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'my-auth-token'
+    })
+};
 
 @Injectable({
     providedIn: 'root'
