28f3b2235901383dbe08a37bd7f886395166a179
Todsphol <Todspholwon@gmail.com>
2018-10-30 23:44:37 +0700

    Fix test file profile spec

--- src/app/layouts/components/profile/profile.component.spec.ts
+++ src/app/layouts/components/profile/profile.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { ProfileComponent } from './profile.component';
 
-describe('ProfileComponent', () => {
+
+fdescribe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      declarations: [ProfileComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
+        })
+      ],
+      providers: []
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
