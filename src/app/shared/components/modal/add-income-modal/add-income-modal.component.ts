134f5c684ee94e8cc36b716a094d12e8b4a88f00
Oak <oak@odds.team>
2018-11-02 16:25:07 +0700

    add-income => auto format currency

--- src/app/shared/components/modal/add-income-modal/add-income-modal.component.ts
+++ src/app/shared/components/modal/add-income-modal/add-income-modal.component.ts
@@ -44,8 +44,13 @@ export class AddIncomeModalComponent implements OnInit {
         setTimeout(() => { this.activeModal.close(); });
     }
 
-    inputIncomeAmount(data: number) {
-        this.addIncome.totalIncome = data.toString();
+    inputIncomeAmount(data: string) {
+        data = data.replace(/[^0-9.]/g, '');
+        data = data.indexOf(',') !== -1 ? data.replace(/,/g, '') : data;
+        this.totalIncome = this.formatInteger(data);
+        this.addIncome.totalIncome = data;
+        console.log(this.addIncome.totalIncome);
+        console.log(this.totalIncome);
     }
 
     inputNote(note: string) {
@@ -55,5 +60,23 @@ export class AddIncomeModalComponent implements OnInit {
     disibleButton(): boolean {
         if (!this.addIncome.totalIncome || Number(this.addIncome.totalIncome) < 1) { return true; }
         return false;
+    }
+
+    xxx(x) {
+        x = x.replace(/[^0-9.]/g, '');
+        x = x.indexOf(',') !== -1 ? x.replace(/,/g, '') : x;
+        this.totalIncome = this.formatInteger(x);
+    }
+
+    validatePureText(value: string) {
+        return /[^0-9.]/.test(value);
+    }
+
+    formatInteger(Result: string): string {
+        Result = Result.substring(0, 9);
+        Result = Result.replace(/^(\d+)(\d{3})/, '$1,$2');
+        Result = Result.replace(/^(\d+)(\d{3})/, '$1,$2');
+        Result = Result.replace(/^(\d+)(\d{3})/, '$1,$2');
+        return Result;
     }
 }
