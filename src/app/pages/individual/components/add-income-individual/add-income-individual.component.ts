3c55b8a53b58e721cd0c5b6fdf80d18a286c7546
Bordin Intarama <bordin@odds.team>
2018-11-08 23:29:44 +0700

    modified individual component

--- src/app/pages/individual/components/add-income-individual/add-income-individual.component.ts
+++ src/app/pages/individual/components/add-income-individual/add-income-individual.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { AddIncomeResponse } from 'src/app/shared/model/add-income-model-response';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { StateService } from 'src/app/core/state.service';
+import { IncomeFlag } from 'src/app/shared/model/income-flag';
 
 @Component({
   selector: 'app-add-income-individual',
@@ -6,10 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-income-individual.component.scss']
 })
 export class AddIncomeIndividualComponent implements OnInit {
+  salary = 0;
+  @ViewChild('templateModal') templateModal: TemplateRef<any>;
+  modalRef: BsModalRef;
+  note = 'อยากได้เงินก็กรอกมาสิ';
+  userFlag: string;
+  addIncomeResponse: AddIncomeResponse;
 
-  constructor() { }
+  constructor(
+    private modalService: BsModalService,
+    private worklogApiService: WorklogApiService,
+    private stateService: StateService
+  ) { }
 
   ngOnInit() {
+    this.worklogApiService.forCheckTokenPleaseRemoveMeIfFlowLoginFinnished().subscribe(() => this.checkStatusUser());
+    this.stateService.isUserFlag.subscribe(flag => {
+      this.userFlag = flag;
+    });
+  }
+
+  checkStatusUser() {
+    this.worklogApiService.getIncomeByUserID().subscribe(res => {
+      IncomeFlag.id = res.id;
+      this.addIncomeResponse = res;
+      this.salary = Number(res.totalIncome);
+      this.note = res.note;
+    });
+  }
+
+  openTemplateModal() {
+    this.openModal(this.templateModal);
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template,
+      Object.assign({}, { ignoreBackdropClick: true, })
+    );
+  }
+
+  closeModalEvent(event) {
+    if (event) {
+      this.closeModal();
+      this.ngOnInit();
+    }
+  }
+
+  closeModal() {
+    this.modalRef.hide();
   }
 
 }
