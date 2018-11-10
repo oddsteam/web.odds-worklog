c96b88e7d92bf67fe3f39d5337e5ae22289b1a5c
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2018-11-10 10:20:38 +0700

    create unittest layouts component test getUserById & setFlagUsers call service

--- src/app/layouts/layouts.component.spec.ts
+++ src/app/layouts/layouts.component.spec.ts
@@ -4,16 +4,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { StateService } from '../core/state.service';
 import { WorklogApiService } from '../core/worklog-api.service';
 import { ProfileComponent } from '../shared/components/profile/profile.component';
 import { TabMenuComponent } from '../shared/components/tab-menu/tab-menu.component';
+import { Users } from '../shared/model/user-model';
 import { LayoutsComponent } from './layouts.component';
 
 
 describe('LayoutsComponent', () => {
   let component: LayoutsComponent;
   let fixture: ComponentFixture<LayoutsComponent>;
-
+  let worklogApiService: WorklogApiService;
+  let stateService: StateService;
+  let mockUsers: Users;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [LayoutsComponent, TabMenuComponent, ProfileComponent],
@@ -21,18 +26,43 @@ describe('LayoutsComponent', () => {
         , TranslateModule.forRoot({
           loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
         })],
-      providers: [WorklogApiService],
+      providers: [WorklogApiService, StateService],
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LayoutsComponent);
+    worklogApiService = TestBed.get(WorklogApiService);
+    stateService = TestBed.get(StateService);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockUsers = <Users>{
+      id: '1234567890',
+      fullnameEn: 'ทดสอบ ชอบลงทุน',
+      email: 'abc@abc.com',
+      bankAccountName: 'ทดสอบ ชอบลงทุน',
+      bankAccountNumber: '0987654321',
+      corporateFlag: 'Y',
+      thaiCitizenId: '1234567890',
+    };
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('shold call method getUserByID to Have Been Called worklogApiService getUserById ', () => {
+    spyOn(worklogApiService, 'getUserByID').and.returnValues(of(mockUsers));
+    spyOn(component, 'setFlagUsers');
+    component.getUserByID();
+    expect(component.setFlagUsers).toHaveBeenCalled();
+    expect(worklogApiService.getUserByID).toHaveBeenCalled();
+
+  });
+
+  it('shold call method setFlagUsers to Have Been Called stateService setFlagUser', () => {
+    spyOn(stateService, 'setFlagUser');
+    component.setFlagUsers('Y');
+    expect(stateService.setFlagUser).toHaveBeenCalled();
   });
 });
