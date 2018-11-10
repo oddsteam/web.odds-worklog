7d8f4e81ae51a71dee4c68084d325e36d8afacee
vasocaity <lazy.yoci11@gmail.com>
2018-11-10 15:36:19 +0700

    add unit test list-corporate component

--- src/app/pages/corporate/components/list-corporate/list-corporate.component.spec.ts
+++ src/app/pages/corporate/components/list-corporate/list-corporate.component.spec.ts
@@ -3,12 +3,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TableListComponent } from 'src/app/shared/components/table-list/table-list.component';
 import { StatusHighlightDirective } from 'src/app/shared/directives/status-highlight.directive';
 import { ListCorporateComponent } from './list-corporate.component';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { of, throwError } from 'rxjs';
+import { StateService } from 'src/app/core/state.service';
 
 
 describe('ListCorporateComponent', () => {
   let component: ListCorporateComponent;
   let fixture: ComponentFixture<ListCorporateComponent>;
-
+  let worklogService: WorklogApiService;
+  let stateService: StateService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ListCorporateComponent, TableListComponent, StatusHighlightDirective],
@@ -19,6 +23,8 @@ describe('ListCorporateComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListCorporateComponent);
+    worklogService = TestBed.get(WorklogApiService);
+    stateService = TestBed.get(StateService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -26,4 +32,108 @@ describe('ListCorporateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call forCheckTokenPleaseRemoveMeIfFlowLoginFinnished in worklog service', () => {
+    spyOn(worklogService, 'forCheckTokenPleaseRemoveMeIfFlowLoginFinnished').and.returnValue(of());
+    component.ngOnInit();
+    expect(worklogService.forCheckTokenPleaseRemoveMeIfFlowLoginFinnished).toHaveBeenCalled();
+  });
+
+  it('forCheckTokenPleaseRemoveMeIfFlowLoginFinnished should be defined', () => {
+    spyOn(stateService, 'listIncomeCorporateTrigger').and.returnValue(of());
+    component.ngOnInit();
+    expect(stateService.listIncomeCorporateTrigger).toBeDefined();
+  });
+
+  it('should call getListIncomeCorporate', () => {
+    spyOn(stateService, 'listIncomeCorporateTrigger').and.returnValue(of());
+    spyOn(component, 'getListIncomeCorporate');
+    component.ngOnInit();
+    expect(component.getListIncomeCorporate).toHaveBeenCalled();
+  });
+
+  it('should call getListIncomeCorporate in worklog service', () => {
+    const mockResponse = {
+      submitDate: '2018-10-09:00:00:00',
+      status: 'Y',
+      user: [
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+      ]
+    };
+    spyOn(worklogService, 'getListIncomeCorporate').and.returnValue(of(mockResponse));
+    component.getListIncomeCorporate();
+    expect(worklogService.getListIncomeCorporate).toHaveBeenCalled();
+  });
+
+  it('listIncome should equal to response data', () => {
+    const mockResponse = {
+      submitDate: '2018-10-09:00:00:00',
+      status: 'Y',
+      user: [
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+      ]
+    };
+    spyOn(worklogService, 'getListIncomeCorporate').and.returnValue(of(mockResponse));
+    component.getListIncomeCorporate();
+    expect(component.listIncome).toEqual(mockResponse);
+  });
+
+  it('should call exportDataCorporate', () => {
+    const mockBlob = new Blob([], { type: 'text/csv;charset=utf-8;' });
+    spyOn(worklogService, 'exportDataCorporate').and.returnValue(of(mockBlob));
+    component.exportCorporate();
+    expect(worklogService.exportDataCorporate).toHaveBeenCalled();
+  });
+
+  it('should call downloadFile if exportDataCorporate return response', () => {
+    const mockBlob = new Blob([], { type: 'text/csv;charset=utf-8;' });
+    spyOn(worklogService, 'exportDataCorporate').and.returnValue(of(mockBlob));
+    spyOn(component, 'downloadFile');
+    component.exportCorporate();
+    expect(component.downloadFile).toHaveBeenCalledWith(mockBlob, 'income_corporate.csv');
+  });
+
+  it('alert message if exportDataCorporate return error', () => {
+    spyOn(worklogService, 'exportDataCorporate').and.callFake(() => {
+      return throwError('Fake Error');
+    });
+    spyOn(window, 'alert');
+    component.exportCorporate();
+    expect(window.alert).toHaveBeenCalledWith(`Can't export corporate income to CSV file.`);
+  });
+
 });
