07d4116443e2357008fdee01652a70c3469a95ab
Todsphol <Todspholwon@gmail.com>
2018-11-13 21:13:09 +0700

    add test in addincome-individual and list-individual

--- src/app/pages/individual/components/add-income-individual/add-income-individual.component.spec.ts
+++ src/app/pages/individual/components/add-income-individual/add-income-individual.component.spec.ts
@@ -4,13 +4,18 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BsModalService, ComponentLoaderFactory, PositioningService } from 'ngx-bootstrap';
+import { of, throwError } from 'rxjs';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
 import { ModalIncomeComponent } from 'src/app/shared/components/modal-income/modal-income.component';
+import { AddIncomeResponse } from 'src/app/shared/model/add-income-model-response';
 import { AddIncomeIndividualComponent } from './add-income-individual.component';
 
 
 describe('AddIncomeIndividualComponent', () => {
   let component: AddIncomeIndividualComponent;
   let fixture: ComponentFixture<AddIncomeIndividualComponent>;
+  let worklogservice: WorklogApiService;
+  let modalService: BsModalService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,11 +28,67 @@ describe('AddIncomeIndividualComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddIncomeIndividualComponent);
+    worklogservice = TestBed.get(WorklogApiService);
+    modalService = TestBed.get(BsModalService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getIncomeByUserID in worklog service', () => {
+    const mockResponse: AddIncomeResponse = {
+      id: '01',
+      userId: '0000022233',
+      totalIncome: '100000',
+      netIncome: '40',
+      submitDate: '2018-10-22:00:00:00',
+      note: '',
+      vat: '0.23',
+      wht: '100'
+    };
+    spyOn(worklogservice, 'getIncomeByUserID').and.returnValue(of(mockResponse));
+    component.checkStatusUser();
+    expect(worklogservice.getIncomeByUserID).toHaveBeenCalled();
+  });
+
+  it('addIncomeResponse should be equal data from service if getIncomeByUserID is not error', () => {
+    const mockResponse: AddIncomeResponse = {
+      id: '01',
+      userId: '0000022233',
+      totalIncome: '100000',
+      netIncome: '40',
+      submitDate: '2018-10-22:00:00:00',
+      note: '',
+      vat: '0.23',
+      wht: '100'
+    };
+    spyOn(worklogservice, 'getIncomeByUserID').and.returnValue(of(mockResponse));
+    component.checkStatusUser();
+    expect(component.addIncomeResponse).toEqual(mockResponse);
+  });
+
+  it('addIncomeResponse should be null if getIncomeByUserID is error', () => {
+    spyOn(worklogservice, 'getIncomeByUserID').and.callFake(() => {
+      return throwError(new Error('Fake error'));
+    });
+    component.checkStatusUser();
+    expect(component.addIncomeResponse).toBeNull();
+  });
+
+  it('should call openModal', () => {
+    spyOn(component, 'openModal');
+    component.openTemplateModal();
+    expect(component.openModal).toHaveBeenCalled();
+  });
+
+  it('', () => {
+    spyOn(component, 'openTemplateModal');
+
+    component.openTemplateModal();
+
+    expect(component.openTemplateModal).toHaveBeenCalled();
   });
 });
--- src/app/pages/individual/components/list-individual/list-individual.component.spec.ts
+++ src/app/pages/individual/components/list-individual/list-individual.component.spec.ts
@@ -1,6 +1,8 @@
 /* tslint:disable:no-unused-variable */
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
 import { TableListComponent } from 'src/app/shared/components/table-list/table-list.component';
 import { StatusHighlightDirective } from 'src/app/shared/directives/status-highlight.directive';
 import { ListIndividualComponent } from './list-individual.component';
@@ -10,6 +12,7 @@ import { DataTablesModule } from 'angular-datatables';
 describe('ListIndividualComponent', () => {
   let component: ListIndividualComponent;
   let fixture: ComponentFixture<ListIndividualComponent>;
+  let worklogService: WorklogApiService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,11 +24,102 @@ describe('ListIndividualComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListIndividualComponent);
+    worklogService = TestBed.get(WorklogApiService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call forCheckTokenPleaseRemoveMeIfFlowLoginFinnished in worklog service', () => {
+    spyOn(worklogService, 'forCheckTokenPleaseRemoveMeIfFlowLoginFinnished').and.returnValue(of());
+    component.ngOnInit();
+    expect(worklogService.forCheckTokenPleaseRemoveMeIfFlowLoginFinnished).toHaveBeenCalled();
+  });
+
+  it('should call getListIncomeIndividual in worklog service', () => {
+    const mockResponse = {
+      submitDate: '2018-10-09:00:00:00',
+      status: 'Y',
+      user: [
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+      ]
+    };
+    spyOn(worklogService, 'getListIncomeIndividual').and.returnValue(of(mockResponse));
+    component.getListIncomeIndividual();
+    expect(worklogService.getListIncomeIndividual).toHaveBeenCalled();
+  });
+
+  it('listIncome should equal to response data', () => {
+    const mockResponse = {
+      submitDate: '2018-10-09:00:00:00',
+      status: 'Y',
+      user: [
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+        {
+          id: '1233',
+          fullnameEn: 'ODDS ODDS',
+          email: 'odds@odds.team',
+          bankAccountName: 'odds odds',
+          bankAccountNumber: '112211221122',
+          corporateFlag: 'Y',
+          thaiCitizenId: '12345423'
+        },
+      ]
+    };
+    spyOn(worklogService, 'getListIncomeIndividual').and.returnValue(of(mockResponse));
+    component.getListIncomeIndividual();
+    expect(component.listIncomeIndividual).toEqual(mockResponse);
+  });
+
+  it('should call exportDataIndividual', () => {
+    const mockBlob = new Blob([], { type: 'text/csv;charset=utf-8;' });
+    spyOn(worklogService, 'exportDataIndividual').and.returnValue(of(mockBlob));
+    component.exportIndividual();
+    expect(worklogService.exportDataIndividual).toHaveBeenCalled();
+  });
+
+  it('should call downloadFile if exportDataIndividual return response', () => {
+    const mockBlob = new Blob([], { type: 'text/csv;charset=utf-8;' });
+    spyOn(worklogService, 'exportDataIndividual').and.returnValue(of(mockBlob));
+    spyOn(component, 'downloadFile');
+    component.exportIndividual();
+    expect(component.downloadFile).toHaveBeenCalledWith(mockBlob, 'income_individual.csv');
+  });
+
+  it('alert message if exportDataIndividual return error', () => {
+    spyOn(worklogService, 'exportDataIndividual').and.callFake(() => {
+      return throwError('Fake Error');
+    });
+    spyOn(window, 'alert');
+    component.exportIndividual();
+    expect(window.alert).toHaveBeenCalledWith(`Can't export individual income to CSV file.`);
   });
 });
