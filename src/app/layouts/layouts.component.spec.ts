5bea5849141581e14a10897747cc346bf01b6b6a
waraporn maensiri <wara.phon@odds.team>
2018-11-24 15:10:01 +0700

    add unit test in layout component

--- src/app/layouts/layouts.component.spec.ts
+++ src/app/layouts/layouts.component.spec.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
@@ -13,23 +13,30 @@ import { Users } from '../shared/model/user-model';
 import { LayoutsComponent } from './layouts.component';
 import { AddIncomeComponent } from '../shared/components/add-income/add-income.component';
 import { ModalIncomeComponent } from '../shared/components/modal-income/modal-income.component';
-import { SharedModule } from '../shared/shared.module';
+import { Router } from '@angular/router';
 
+class MockRouterService {
+  navigate() { }
+}
+const mockRouterService = new MockRouterService();
 
 describe('LayoutsComponent', () => {
   let component: LayoutsComponent;
   let fixture: ComponentFixture<LayoutsComponent>;
   let worklogApiService: WorklogApiService;
-  let stateService: StateService;
   let mockUsers: Users;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LayoutsComponent, TabMenuComponent, ProfileComponent, AddIncomeComponent],
-      imports: [CommonModule, FormsModule, RouterTestingModule, HttpClientTestingModule
+      declarations: [LayoutsComponent, TabMenuComponent, ProfileComponent, AddIncomeComponent, ModalIncomeComponent],
+      imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterTestingModule, HttpClientTestingModule
         , TranslateModule.forRoot({
           loader: { provide: TranslateLoader, useClass: TranslateFakeLoader }
         })],
-      providers: [WorklogApiService, StateService],
+      providers: [WorklogApiService, StateService,
+        {
+          provide: Router,
+          useValue: mockRouterService,
+        }],
     })
       .compileComponents();
   }));
@@ -37,7 +44,6 @@ describe('LayoutsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LayoutsComponent);
     worklogApiService = TestBed.get(WorklogApiService);
-    stateService = TestBed.get(StateService);
     component = fixture.componentInstance;
     mockUsers = <Users>{
       id: '1234567890',
@@ -50,19 +56,38 @@ describe('LayoutsComponent', () => {
     };
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-  // it('should call method getUserByID to Have Been Called worklogApiService getUserById ', () => {
-  //   spyOn(worklogApiService, 'getUserByID').and.returnValues(of(mockUsers));
-  //   component.getUserByID();
-  //   expect(worklogApiService.getUserByID).toHaveBeenCalled();
+  it('should call method getLogin to Have Been Called worklogApiService getUserById ', () => {
+    spyOn(worklogApiService, 'getLogin').and.returnValues(of(mockUsers));
+    component.ngOnInit();
+    expect(worklogApiService.getLogin).toHaveBeenCalled();
+  });
 
-  // });
+  it('should sessionStorage token equal by response from worklogApiService', () => {
+    const mockToken = {
+      token: '.eyJ1c2VySWQiOiJbyZVcdWZmZmQ6Mlx1MDAwNFx1ZmZmZGBcdWZmZmRcclx1ZmZ'
+    };
+    spyOn(worklogApiService, 'getLogin').and.returnValues(of(mockToken));
+    component.ngOnInit();
+    expect(sessionStorage.getItem('token')).toEqual('Bearer ' + mockToken.token);
+  });
 
-  // it('should call method setFlagUsers to Have Been Called stateService setFlagUser', () => {
-  //   spyOn(stateService, 'setFlagUser');
-  //   expect(stateService.setFlagUser).toHaveBeenCalled();
-  // });
+  it('should call goToPage when worklogApiService return response', () => {
+    const mockToken = {
+      token: '.eyJ1c2VySWQiOiJbyZVcdWZmZmQ6Mlx1MDAwNFx1ZmZmZGBcdWZmZmRcclx1ZmZ'
+    };
+    spyOn(worklogApiService, 'getLogin').and.returnValues(of(mockToken));
+    spyOn(component, 'goToPage');
+    component.ngOnInit();
+    expect(component.goToPage).toHaveBeenCalled();
+  });
+
+  it('should navigate to /corporate when call goToPage', () => {
+    spyOn(mockRouterService, 'navigate');
+    component.goToPage();
+    expect(mockRouterService.navigate).toHaveBeenCalledWith([`/corporate`]);
+  });
 });
