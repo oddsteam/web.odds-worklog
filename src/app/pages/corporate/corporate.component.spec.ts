7413437b1745a17c75400ddd0ec760778863a447
Todsphol <Todspholwon@gmail.com>
2018-11-27 23:21:33 +0700

    add test in corporate component and individual component

--- src/app/pages/corporate/corporate.component.spec.ts
+++ src/app/pages/corporate/corporate.component.spec.ts
@@ -1,28 +1,57 @@
 /* tslint:disable:no-unused-variable */
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ContentLoaderModule } from '@netbasal/content-loader';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OrderModule } from 'ngx-order-pipe';
+import { of } from 'rxjs';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { AddIncomeComponent } from 'src/app/shared/components/add-income/add-income.component';
+import { ModalIncomeComponent } from 'src/app/shared/components/modal-income/modal-income.component';
 import { TableListComponent } from 'src/app/shared/components/table-list/table-list.component';
 import { StatusHighlightDirective } from 'src/app/shared/directives/status-highlight.directive';
-import { CorporateComponent } from './corporate.component';
 import { ListCorporateComponent } from './components/list-corporate/list-corporate.component';
+import { CorporateComponent } from './corporate.component';
 
 
 describe('CorporateComponent', () => {
   let component: CorporateComponent;
   let fixture: ComponentFixture<CorporateComponent>;
+  let worklogApiService: WorklogApiService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [CorporateComponent, TableListComponent, StatusHighlightDirective, ListCorporateComponent],
-      imports: [NgbModule.forRoot(), HttpClientTestingModule]
+      declarations: [CorporateComponent, TableListComponent, StatusHighlightDirective, ListCorporateComponent,
+        AddIncomeComponent, ModalIncomeComponent],
+      imports: [NgbModule.forRoot(), HttpClientTestingModule, OrderModule, ContentLoaderModule,
+        FormsModule, ReactiveFormsModule],
+      providers: [WorklogApiService]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CorporateComponent);
+    worklogApiService = TestBed.get(WorklogApiService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be personType to equal Y when coporateFlag is Y', () => {
+    const res = {
+      corporateFlag: 'Y'
+    };
+    spyOn(worklogApiService, 'getUserByID').and.returnValue(of(res));
+
+    component.ngOnInit();
+
+    expect(component.personType).toEqual('Y');
+    expect(worklogApiService.getUserByID).toHaveBeenCalled();
+  });
+
 });
--- src/app/pages/individual/individual.component.spec.ts
+++ src/app/pages/individual/individual.component.spec.ts
@@ -1,33 +1,38 @@
 /* tslint:disable:no-unused-variable */
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ContentLoaderModule } from '@netbasal/content-loader';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OrderModule } from 'ngx-order-pipe';
+import { of } from 'rxjs';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { AddIncomeComponent } from 'src/app/shared/components/add-income/add-income.component';
+import { ModalIncomeComponent } from 'src/app/shared/components/modal-income/modal-income.component';
 import { TableListComponent } from 'src/app/shared/components/table-list/table-list.component';
 import { StatusHighlightDirective } from 'src/app/shared/directives/status-highlight.directive';
 import { ListIndividualComponent } from './components/list-individual/list-individual.component';
 import { IndividualComponent } from './individual.component';
-import { OrderModule } from 'ngx-order-pipe';
-import { ContentLoaderModule } from '@netbasal/content-loader';
-import { AddIncomeComponent } from 'src/app/shared/components/add-income/add-income.component';
-import { ModalIncomeComponent } from 'src/app/shared/components/modal-income/modal-income.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 describe('IndividualComponent', () => {
   let component: IndividualComponent;
   let fixture: ComponentFixture<IndividualComponent>;
+  let worklogApiService: WorklogApiService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [IndividualComponent, TableListComponent, StatusHighlightDirective,
         ListIndividualComponent, AddIncomeComponent, ModalIncomeComponent],
       imports: [NgbModule.forRoot(), HttpClientTestingModule, ContentLoaderModule, OrderModule,
-      FormsModule, ReactiveFormsModule],
+        FormsModule, ReactiveFormsModule],
+      providers: [WorklogApiService]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
+    worklogApiService = TestBed.get(WorklogApiService);
     fixture = TestBed.createComponent(IndividualComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -35,5 +40,17 @@ describe('IndividualComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be personType to equal Y when coporateFlag is Y', () => {
+    const res = {
+      corporateFlag: 'N'
+    };
+    spyOn(worklogApiService, 'getUserByID').and.returnValue(of(res));
+
+    component.ngOnInit();
+
+    expect(component.personType).toEqual('N');
+    expect(worklogApiService.getUserByID).toHaveBeenCalled();
   });
 });
