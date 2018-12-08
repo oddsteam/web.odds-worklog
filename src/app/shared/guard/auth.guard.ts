36e99d612bf7adbb31490065e67d7f5fdbf214b3
Suphakrit Nakhabat <suphakrit@odds.team>
2018-12-09 02:49:21 +0700

    Update auth guard

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -2,24 +2,26 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { WorklogApiService } from '../../core/worklog-api.service';
-import { FirstLogin } from '../model/user-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  fistLogin: FirstLogin;
   constructor(private router: Router, private worklogApiService: WorklogApiService) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    this.fistLogin = this.worklogApiService.getFirstLogin();
-    console.log('test', this.fistLogin);
-    if (sessionStorage.getItem('token')) {
-      if (this.fistLogin.firstLogin === 'Y') {
+    const userId = sessionStorage.getItem('idUser');
+    if (sessionStorage.getItem('token') && userId) {
+      this.worklogApiService.getUserByID(userId).subscribe(res => {
+        if (res.firstName === '' || res.lastName === '') {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      }, err => {
         this.router.navigate(['/login']);
         return false;
-      }
+      });
       return true;
     }
     this.router.navigate(['/login']);
