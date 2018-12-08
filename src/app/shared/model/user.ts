2260f0257b6ed92321082c6b4595f4e9d5c5f92b
Suphakrit Nakhabat <suphakrit@odds.team>
2018-12-09 02:49:45 +0700

    Update model

---
+++ src/app/shared/model/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    id: string;
+    role: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    bankAccountName: string;
+    bankAccountNumber: string;
+    thaiCitizenId: string;
+    vat: string;
+    slackAccount: string;
+    transcript: string;
+    siteId: string;
+}
