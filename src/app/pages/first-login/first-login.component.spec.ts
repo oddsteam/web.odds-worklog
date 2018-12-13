519a9002efd0b8be9f97175597d4094c01b6ef0d
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2018-12-13 21:21:09 +0700

    create unittest getListSite page firstLogin

--- src/app/pages/first-login/first-login.component.spec.ts
+++ src/app/pages/first-login/first-login.component.spec.ts
@@ -5,6 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { FirstLoginComponent } from './first-login.component';
 import { WorklogApiService } from '../../core/worklog-api.service';
 import { of } from 'rxjs';
+import { Site } from '../../shared/model/site';
 
 describe('FirstLoginComponent', () => {
   let component: FirstLoginComponent;
@@ -45,5 +46,36 @@ describe('FirstLoginComponent', () => {
     expect(component.user.bankAccountNumber).toEqual('1234567890');
     expect(component.user.slackAccount).toEqual('test@odds.team');
     expect(worklogapiService.updateUser).toHaveBeenCalled();
+  });
+
+  it('should call method getListSite call service worklogAPIService getSitesData', () => {
+    const mockListSite: Site[] = [{
+      id: '5c0fb860f37e2f8698989cdd',
+      name: 'SEC'
+    },
+    {
+      id: '5c0fb875f37e2f8698989cde',
+      name: 'SET'
+    },
+    {
+      id: '5c0fb87df37e2f8698989cdf',
+      name: 'KTB'
+    },
+    {
+      id: '5c0fb885f37e2f8698989ce0',
+      name: 'KBTG'
+    },
+    {
+      id: '5c0fb88af37e2f8698989ce1',
+      name: 'DTAC'
+    }];
+    spyOn(worklogapiService, 'getSitesData').and.returnValue(of(mockListSite));
+    component.getListSite();
+    expect(worklogapiService.getSitesData).toHaveBeenCalled();
+    expect(component.siteList[0].name).toEqual('SEC');
+    expect(component.siteList[1].name).toEqual('SET');
+    expect(component.siteList[2].name).toEqual('KTB');
+    expect(component.siteList[3].name).toEqual('KBTG');
+    expect(component.siteList[4].name).toEqual('DTAC');
   });
 });
