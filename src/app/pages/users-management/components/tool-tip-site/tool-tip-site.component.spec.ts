000f7ec828a3a5414f80c4af4e029804b43497f4
Todsphol <Todspholwon@gmail.com>
2018-12-23 21:22:49 +0700

    Add test in tool-tip-site component

--- src/app/pages/users-management/components/tool-tip-site/tool-tip-site.component.spec.ts
+++ src/app/pages/users-management/components/tool-tip-site/tool-tip-site.component.spec.ts
@@ -1,9 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ToolTipSiteComponent } from './tool-tip-site.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ToolTipSiteComponent } from './tool-tip-site.component';
+
 
 describe('ToolTipSiteComponent', () => {
   let component: ToolTipSiteComponent;
@@ -11,10 +11,10 @@ describe('ToolTipSiteComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToolTipSiteComponent ],
+      declarations: [ToolTipSiteComponent],
       imports: [NgbModule.forRoot(), HttpClientTestingModule, SharedModule],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,5 +25,35 @@ describe('ToolTipSiteComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('When an action is made, toggleTooltip function is called ', () => {
+    const event = <MouseEvent>{
+      target: <Object>{
+      }
+    };
+
+    component.toggleTooltip(event);
+
+    expect(component.clickedElement).toEqual(event.target);
+    expect(component.isShowTooltip).toBeTruthy();
+  });
+
+  it('When an action is made, disableTooltip function is called', () => {
+    component.isShowTooltip = true;
+    const event = <MouseEvent>{
+      target: <Object>{
+      }
+    };
+
+    component.disableTooltip(event);
+
+    expect(component.isShowTooltip).toBeFalsy();
+  });
+
+  it('When an action is made, onSelectedListSite function is called', () => {
+    const site = 'SEC';
+    component.onSelectedListSite(site);
+    expect(component.site).toEqual(site);
   });
 });
