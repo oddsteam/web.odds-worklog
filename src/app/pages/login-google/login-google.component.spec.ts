c9d5cc96a67210b8e930b4e320fcfadf95952db6
Suphakrit Nakhabat <suphakrit@odds.team>
2019-01-05 18:37:07 +0700

    Fix ng test [object ErrorEvent] thrown

--- src/app/pages/login-google/login-google.component.spec.ts
+++ src/app/pages/login-google/login-google.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService, AuthServiceConfig } from 'angular-6-social-login';
-import { of } from 'rxjs/internal/observable/of';
+import { of } from 'rxjs';
 import { getAuthServiceConfigs } from '../../app.module';
 import { WorklogApiService } from '../../core/worklog-api.service';
 import { LoginGoogleComponent } from './login-google.component';
