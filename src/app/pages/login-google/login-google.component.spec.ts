d1d04eba7b0f45cce76416c55339ddadf62719bc
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2019-01-05 16:44:10 +0700

    fix unittest login-google component

--- src/app/pages/login-google/login-google.component.spec.ts
+++ src/app/pages/login-google/login-google.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService, AuthServiceConfig } from 'angular-6-social-login';
+import { of } from 'rxjs/internal/observable/of';
 import { getAuthServiceConfigs } from '../../app.module';
 import { WorklogApiService } from '../../core/worklog-api.service';
 import { LoginGoogleComponent } from './login-google.component';
@@ -30,25 +31,25 @@ describe('LoginGoogleComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
     workLogService = TestBed.get(WorklogApiService);
-    socialAuthService = TestBed.get(AuthService);
+  });
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  // it('should be call method socialSignIn loginwithGoogle', inject([Router], (router: Router)  => {
-  //   const res = {
-  //     token: '1234567890',
-  //     firstLogin: 'Y',
-  //     idUser: '1234567890'
-  //   };
-  //   const dataPromiss = {
-  //     idToken: '1234567890'
-  //   };
-  //   spyOn(socialAuthService, 'signIn').and.returnValue(Promise.resolve(dataPromiss));
-  //   spyOn(workLogService, 'getLoginGoogle').and.returnValue(of(res));
-  //   spyOn(router, 'navigate');
-  //   component.socialSignIn();
-  //   expect(router.navigate).toHaveBeenCalledWith(['firstlogin']);
-
-  // }));
+  it('should be call method socialSignIn loginwithGoogle', () => {
+    socialAuthService = TestBed.get(AuthService);
+    const res = {
+      token: '1234567890',
+      firstLogin: 'Y',
+      idUser: '1234567890'
+    };
+    const dataPromiss = {
+      idToken: '1234567890'
+    };
+    spyOn(socialAuthService, 'signIn').and.returnValue(Promise.resolve(dataPromiss));
+    spyOn(workLogService, 'getLoginGoogle').and.returnValue(of(res));
+    component.socialSignIn();
+  });
 
   // it('should set individualListed, corporateListed when call cacheData()', () => {
   //   spyOn(workLogService, 'getListIncomeIndividual').and.returnValue(of(''));
