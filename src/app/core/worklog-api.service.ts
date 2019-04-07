a064d9241a8360921b4bf5bc03eadb88d545655a
Todsphol <Todspholwon@gmail.com>
2019-04-07 17:15:28 +0700

    create new function exportDataDifferentCorporate and exportDataDifferentIndividuals in worklog-api service

--- src/app/core/worklog-api.service.ts
+++ src/app/core/worklog-api.service.ts
@@ -135,8 +135,26 @@ export class WorklogApiService {
         });
     }
 
+    exportDataDifferentCorporate(): Observable<Blob> {
+        return this.http.get(`${this.apiPath}incomes/export/corporate/different`, {
+            headers: new HttpHeaders({
+                Authorization: sessionStorage.getItem('token')
+            }),
+            responseType: 'blob'
+        });
+    }
+
     exportDataIndividual(beforeMonth: string): Observable<Blob> {
         return this.http.get(`${this.apiPath}incomes/export/individual/${beforeMonth}`, {
+            headers: new HttpHeaders({
+                Authorization: sessionStorage.getItem('token')
+            }),
+            responseType: 'blob'
+        });
+    }
+
+    exportDataDifferentIndividuals(): Observable<Blob> {
+        return this.http.get(`${this.apiPath}incomes/export/individual/different`, {
             headers: new HttpHeaders({
                 Authorization: sessionStorage.getItem('token')
             }),
