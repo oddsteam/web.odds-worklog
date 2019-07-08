98abedab22a3aa89d33fe64be03a8c4e4f9486bc
ice <niyana_ice@hotmail.com>
2019-07-08 11:12:25 +0700

    add table show history

--- src/app/pages/history/history.component.spec.ts
+++ src/app/pages/history/history.component.spec.ts
@@ -1,25 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HistoryComponent } from './history.component';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { AddIncomeResponse } from 'src/app/shared/model/add-income-model-response';
+import { of, throwError } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HistoryComponent', () => {
   let component: HistoryComponent;
   let fixture: ComponentFixture<HistoryComponent>;
+  let worklogservice: WorklogApiService;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HistoryComponent ]
+      declarations: [HistoryComponent],
+      imports: [HttpClientTestingModule],
+      providers: [WorklogApiService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HistoryComponent);
     component = fixture.componentInstance;
+    worklogservice = TestBed.get(WorklogApiService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WorklogApiService } from 'src/app/core/worklog-api.service';
+import { AddIncomeResponse } from 'src/app/shared/model/add-income-model-response';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  id = sessionStorage.getItem('idUser');
+  incomeResponse: AddIncomeResponse;
+  isNoData = false;
+  isNoIndividual = false;
 
-  constructor() { }
+  constructor(
+    private worklogApiService: WorklogApiService,
+  ) { }
 
   ngOnInit() {
+    this.getUserById();
+    this.getIncome();
   }
 
+  getUserById() {
+    this.worklogApiService.getUserByID(this.id).subscribe(res => {
+      res.role === 'individual' ? this.isNoIndividual = true : this.isNoIndividual = false;
+    });
+  }
+
+  getIncome() {
+    this.worklogApiService.getIncomeAllMonthByUserID(this.id).subscribe(res => {
+      if (res !== null) {
+        this.incomeResponse = res;
+        this.isNoData = true;
+
+        const array: any = res;
+        array.forEach(value => {
+          value.totalIncome = this.calNetIncome(value.totalIncome, value.vat, value.wht);
+        });
+      } else { this.isNoData = false; }
+    });
+  }
+
+  checkValue(data: string): string {
+    return data.trim() === '' ? '-' : data.trim();
+  }
+
+  calNetIncome(totalIncome: string, vat: string, wht: string): string {
+    return (
+      this.stringToNumber(totalIncome) +
+      this.stringToNumber(vat) -
+      this.stringToNumber(wht)
+    ).toString();
+  }
+
+  stringToNumber(text: string): number {
+    return Number(this.cutComma(text));
+  }
+
+  cutComma(text: string): string {
+    return text.replace(/,/g, '');
+  }
 }
