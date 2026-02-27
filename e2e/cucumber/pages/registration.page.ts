d0fb62ea81cb0fe1c352fdc9385a197e7e2c4cb7
Chokchai Phatharamalai <juacompe@gmail.com>
2026-02-28 00:16:36 +0700

    try to fix build
    extend timeout again

--- e2e/cucumber/pages/registration.page.ts
+++ e2e/cucumber/pages/registration.page.ts
@@ -57,7 +57,7 @@ export class RegistrationPage {
     await this.page.getByRole("button", { name: "Save" }).click();
     await this.page.waitForURL(
       (url) => url.origin === APP_URL && !url.pathname.includes("/firstlogin"),
-      { timeout: 30000 }
+      { timeout: 45000 }
     );
   }
 
